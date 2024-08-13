220a8e1e3be6f20e166030cb68353a7b447d49cd
Hasan Borshalli <hasanborshalli@gmail.com>
2024-08-13 20:14:46 +0300

    last change

--- index.js
+++ index.js
@@ -3,15 +3,20 @@ const articles = [
     {
         title: "Understanding the difference between grid-template and grid-auto",
         content:
-            "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nisidolor harum consequuntur libero excepturi natus, culpa placeatatque ut a consequatur ipsam architecto ducimus animi quibusdam,perspiciatis explicabo odit aliquid.",
+            "In CSS Grid Layout, grid-template and grid-auto serve different purposes. The grid-template property is used to explicitly define the rows, columns, and areas of a grid. For example, grid-template-rows and grid-template-columns set fixed sizes for the grid’s rows and columns.",
         date: "Oct 09,2018",
     },
     {
-        title: "Understanding the difference between grid-template and grid-auto",
+        title: "What is the difference between flex and grid?",
         content:
-            "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nisidolor harum consequuntur libero excepturi natus, culpa placeatatque ut a consequatur ipsam architecto ducimus animi quibusdam,perspiciatis explicabo odit aliquid.",
-
-        date: "Oct 09,2018",
+            "Both Flexbox and CSS Grid are powerful layout systems in CSS, but they are designed for different purposes and use cases.",
+        date: "Sep 12,2020",
+    },
+    {
+        title: "How animations are used?",
+        content:
+            "CSS animations allow you to animate transitions between different styles or states of an element over time. Animations in CSS are created using keyframes and properties that control the timing, duration, and effects of the animation.",
+        date: "Oct 21,2020",
     },
 ];
 const nbPosts = document.getElementById("nb-posts"); //this is used to determine the number of words found
@@ -41,27 +46,23 @@ function searchWord() {
         const contentElement = article.querySelector("p");
         const title = titleElement.textContent;
         const content = contentElement.textContent;
+        titleElement.innerHTML = highlightText(title, input);
+        contentElement.innerHTML = highlightText(content, input);
         if (
-            title.toLowerCase().includes(input) ||
-            content.toLowerCase().includes(input)
+            titleElement.querySelectorAll(".highlight").length !== 0 ||
+            contentElement.querySelectorAll(".highlight").length !== 0
         ) {
-            titleElement.innerHTML = highlightText(title, input);
-            contentElement.innerHTML = highlightText(content, input);
+            count++;
         }
     });
-    // we will count how many spans with class highlight to identify words found
-    const highlighted = document.querySelectorAll(".highlight");
-    for (let i = 0; i < highlighted.length; i++) {
-        count++;
-    }
     if (count > 0) {
-        nbPosts.innerHTML = "<strong>" + count + " posts </strong>found";
+        nbPosts.innerHTML = "<strong>" + count + " articles </strong>found";
     } else {
         nbPosts.innerHTML = "";
     }
 }
-function highlightText(text, search, count) {
-    if (!search) return text; //extra check if search term is not found
+function highlightText(text, search) {
+    if (!search) return text; //check if search term is not found
     const regex = new RegExp(`(${search})`, "gi"); // this means global and ignore case
     return text.replace(regex, '<span class="highlight">$1</span>'); //this will put the word in a span having class highlight
 }
